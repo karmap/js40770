4acc52ea4949dfd01b9c330c3960aaef55c3ec7d
Adrian Escalante <adresc@gmail.com>
2022-06-27 21:44:01 -0500

    first commit

---
+++ js/clase01_01.js
@@ -0,0 +1,13 @@
+// Empieza mi código de JS
+
+/*
+Esto también es comentario
+*/
+
+let a = 10
+let b = 50
+
+let sum = a + b
+
+console.log('un mensaje a la consola');
+console.log('el resultado es: ' + sum);
---
+++ js/clase01_02.js
@@ -0,0 +1,15 @@
+console.log('Este es el segundo ejemplo');
+
+// alert('Hola mundo')
+
+let gradeA = parseInt( prompt('Calificación de matemáticas') )
+console.log('La calificación de Matemáticas es: ' + gradeA);
+
+let gradeB = parseInt( prompt('Calificación de química') )
+console.log('La calificación de química es: ' + gradeB);
+
+let gradeC = parseInt( prompt('Calificación de física') )
+console.log('La calificación de física es: ' + gradeC);
+
+let average = (gradeA + gradeB + gradeC) / 3
+alert('Tu promedio es: ' + average);
